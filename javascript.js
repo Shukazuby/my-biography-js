b9a70cb5e7f1ea02eb4d79c395de2703f6c7e8cd
Shukazuby <shukazuby@gmail.com>
2023-05-12 02:59:44 +0100

    added files

---
+++ javascript.js
@@ -0,0 +1,5 @@
+let updateMeButtonEl = document.querySelector ('#btn')
+let learningStatus = document.querySelector ('p')
+function updateMeButton () {
+learningStatus.innerHTML= 'I am currently learning javascript'
+}
