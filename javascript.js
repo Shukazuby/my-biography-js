d8abb80f5d8851f6488b14629457d5bdd48372ad
Shukazuby <shukazuby@gmail.com>
2023-05-12 16:39:41 +0100

    made modification to my files

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 let updateMeButtonEl = document.querySelector ('#btn')
-let learningStatus = document.querySelector ('p')
+let learningStatus = document.querySelector ('.zubby')
 function updateMeButton () {
 learningStatus.innerHTML= 'I am currently learning javascript'
 }
